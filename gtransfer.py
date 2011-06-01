6131f666fef03406ef2273f0cc597bda296cbe6c
Julien Graglia <jgr@jgr-pc.(none)>
2011-06-01 09:39:30 +0200

    transfer docs among accounts

--- gtransfer.py
+++ gtransfer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # https://github.com/jgraglia/Google-Docs-Backup
-# Usage : python gdocsbackup.py -l xxx@xxxx.com [-p password]
+# Usage : python gtransfer.py -l xxx@xxxx.com -o yyy@yyyyy.com
 
 """
     This program is free software: you can redistribute it and/or modify
@@ -21,7 +21,6 @@
 # Google Docs API : 
 # http://gdata-python-client.googlecode.com/svn/trunk/pydocs/gdata.docs.data.html#DocsEntry
 
-# see 	GDataCopier, http://gdatacopier.googlecode.com/
 try:
 	import sys
 	import argparse
@@ -55,7 +54,7 @@ except:
         exit(1)
 
 __update_url="https://github.com/jgraglia/Google-Docs-Backup/raw/master/gtransfer.py"
-__version=0.1
+__version=0.2
 	
 # copy from : GDataCopier, http://gdatacopier.googlecode.com/
 # windows problem :  "|*><?
@@ -104,85 +103,18 @@ def setup_logger(options):
     #LOG.warning("test : WARNING message")
     #LOG.error("test : ERROR message")
 
-
-def transferDocument(client, stdToken, spreadsheetToken, feed, storeFolder, storeFlat, ignoreDualCollections):
-	if not feed.entry:
-		LOG.info ("No entries in feed - Nothing to backup")
-	cleanStoreFolder(storeFolder)
-	forceFolder(storeFolder)
-	stats = {'copy':0, 'rejected':0, 'error':0}
-	for entry in feed.entry:
-		ext = ".pdf"
-		dl=False
-
-		LOG.info ("---\"%s\" de type %s"%(entry.title.text.encode(sys.getfilesystemencoding()), entry.GetDocumentType()))
-		# regular expression to parse RFC3389
-		updated_time = datetime.datetime(*map(int, re.split('[^\d]', entry.updated.text)[:-1]))
-		date_string = updated_time.strftime('%b %d %Y %H:%M')
-		LOG.info ("   |- Timestamp : %s"%date_string)
-		#rights
-		LOG.info ("   |- Rights :")
-		try:
-			client.auth_token = stdToken
-			aclFeed = client.GetAclPermissions(entry.resource_id.text)
-			isOwner=False
-			isWriter=False
-			for acl in aclFeed.entry:
-				LOG.info ('   |      |- '+str(acl.scope.value)+' ('+str(acl.scope.type)+') is '+str(acl.role.value))
-				if acl.role.value == "owner" and acl.scope.value== login:
-					isOwner=True
-				if acl.role.value == "writer" and acl.scope.value== login:
-					isWriter=True
-			if isOwner:
-				continue
-			if not isWriter:
-				continue
-		except gdata.client.Unauthorized as error:
-			LOG.error("   |  |- CAN'T DISPLAY ACL FOR DOCUMENT : {0}".format(error))
-			stats['error']+=1
-			continue
-
-		# copy only for doc, spreadsheet and presentation type. See :
-		# http://code.google.com/intl/fr/apis/documents/docs/3.0/developers_guide_python.html#CopyingDocs
-		toCopy=False
-		if entry.GetDocumentType()== "document" :
-			toCopy=True
-		elif entry.GetDocumentType() == "presentation":
-			toCopy=True
-		elif entry.GetDocumentType() == "spreadsheet":
-			toCopy=True
-		elif entry.GetDocumentType() == "drawing":
-			toCopy=True
-		if toCopy: 
-			duplicated_entry = client.Copy(entry, 'TRANSFER_'+entry.title.text.encode(sys.getfilesystemencoding()))
-			LOG.info("Copy done : "+str(duplicated_entry.title.text.encode(sys.getfilesystemencoding())))
-			stats['copy']+=1
-			LOG.info("adding ACL (except mine)")
-			client.auth_token = stdToken
-			aclFeed = client.GetAclPermissions(entry.resource_id.text)
-			for acl in aclFeed.entry:
-				if acl.scope.value== login:
-					continue
-				if acl.role.value== "owner":
-					continue
-				LOG.info (' adding acl to copy doc: '+str(acl.scope.value)+' ('+str(acl.scope.type)+') is '+str(acl.role.value))
-				newScope = gdata.acl.data.AclScope(value=str(acl.scope.value), type='user')
-				newRole = gdata.acl.data.AclRole(value=str(acl.role.value))
-				newAcl_entry = gdata.docs.data.Acl(scope=newScope, role=newRole)
-				created_acl_entry = client.Post(newAcl_entry, duplicated_entry.GetAclLink().href)
-			LOG.info("renaming orig")
-			orig=entry.title.text
-			entry.title.text="TOREMOVE_"+entry.title.text
-			client.Update(entry)
-			LOG.info("renaming copy "+orig)
-			duplicated_entry.title.text=orig
-			client.Update(duplicated_entry, force=True)
-		else:
-			LOG.info("Cant' copy that kind of document : "+entry.GetDocumentType())
-			stats['rejected']+=1
-
-	LOG.info ("Stats : "+str(stats))
-	return stats
+#not all doc type are support for Copy. see Google API
+def canTransferOwnership(entry):
+	if entry.GetDocumentType()== "document" :
+		return True
+	elif entry.GetDocumentType() == "presentation":
+		return True
+	elif entry.GetDocumentType() == "spreadsheet":
+		return True
+	elif entry.GetDocumentType() == "drawing":
+		return True
+	else :
+		return False
 
 def computeFileNameFor(entry, ext):
 	return sanatize_filename(entry.title.text)+ext
@@ -224,31 +156,77 @@ def isOwnerOfFolder(folderAsLink, login, stdToken, spreadsheetToken):
 		LOG.error ("Error: {0}".format(error))
 		return False	
 
-def getFirstCollectionFolderFor(client, stdToken, spreadsheetToken, storeFolder, entry, ignoreDualCollections):
-	firstOwnedFolder=None
-	for folder in entry.InFolders():
-		if isOwnerOfFolder(folder, login, stdToken, spreadsheetToken):
-			if firstOwnedFolder!= None:
-				if ignoreDualCollections:
-					LOG.warning ("           ATTENTION : "+entry.title.text.encode(sys.getfilesystemencoding())+"' stocké dans (au moins) 2 collections vous appartenant : ceci n'est pas géré! "+" : "+folder.title + " & "+ firstOwnedFolder.title)
-					logInReportFile(storeFolder, "\""+entry.title.text.encode(sys.getfilesystemencoding())+"\"")
-					logInReportFile(storeFolder, " se trouvant votre collection ")
-					logInReportFile(storeFolder, "\""+firstOwnedFolder.title+"\"")
-					logInReportFile(storeFolder, " doit aussi être stocké dans la collection ")
-					logInReportFile(storeFolder, "\""+folder.title+"\"")
-					logInReportFile(storeFolder, " vous appartenant elle aussi")
-					logInReportFile(storeFolder, "\n")
-				else:	
-					raise Exception("ERROR ! Document '"+entry.title.text.encode(sys.getfilesystemencoding())+"' stocké dans (au moins) 2 collections vous appartenant : ceci n'est pas géré! "+" : "+folder.title + " & "+ firstOwnedFolder.title)
-			else:
-				firstOwnedFolder = folder;
-		else:
-			logInReportFile(storeFolder, "\""+entry.title.text.encode(sys.getfilesystemencoding())+"\"")
-			logInReportFile(storeFolder, " vous appartient, mais est stocké dans la collection partagée  ")
-			logInReportFile(storeFolder, "\""+folder.title+"\"")
-			logInReportFile(storeFolder, ". Vous devrez réimporter manuellement ce fichier dans cette collection partagée.")
-			logInReportFile(storeFolder, "\n")
-	return firstOwnedFolder
+
+def addWriterShare(client, entry, login):
+	aclFeed = client.GetAclPermissions(entry.resource_id.text)
+	needAdd=True
+	for acl in aclFeed.entry:
+		if acl.role.value == "writer" and acl.scope.value== login:
+			needAdd=False
+			break
+	if needAdd:
+		LOG.info ("   - Adding WRITER share to "+login+" on "+entry.title.text.encode(sys.getfilesystemencoding()))
+		if not args.dryRun:
+			newScope = gdata.acl.data.AclScope(value=login, type='user')
+			newRole = gdata.acl.data.AclRole(value="writer")
+			newAcl_entry = gdata.docs.data.Acl(scope=newScope, role=newRole)
+			created_acl_entry = client.Post(newAcl_entry, entry.GetAclLink().href)
+		stats['addwriter']+=1
+
+def removeAllRightsExceptMine(client, entry, targetLogin):
+	LOG.info ("   - Removing ALL rights of "+targetLogin+" on "+entry.title.text.encode(sys.getfilesystemencoding()))
+	aclFeed = client.GetAclPermissions(entry.resource_id.text)
+	for acl in aclFeed.entry:
+		if not acl.scope.value==targetLogin:
+			LOG.info("        removing acl : "+str(acl.scope.value)+' ('+str(acl.scope.type)+') is '+str(acl.role.value)+' of '+entry.title.text.encode(sys.getfilesystemencoding()))
+			if not args.dryRun:
+				client.Delete(acl, force=True)
+			stats['step4']+=1
+
+def removeAllRightsIfNotOwned(client, entry, targetLogin):
+	if not isOwner(client, entry, targetLogin):
+		LOG.info ("   - Removing ALL rights of "+targetLogin+" on "+entry.title.text.encode(sys.getfilesystemencoding()))
+		aclFeed = client.GetAclPermissions(entry.resource_id.text)
+		for acl in aclFeed.entry:
+			if acl.scope.value==targetLogin:
+				LOG.info("        removing acl : "+str(acl.scope.value)+' ('+str(acl.scope.type)+') is '+str(acl.role.value)+' of '+entry.title.text.encode(sys.getfilesystemencoding()))
+				if not args.dryRun:
+					client.Delete(acl, force=True)
+				stats['removeoldownerright']+=1
+	else:
+		LOG.debug("   - Keeping safe owner doc of "+targetLogin+" : "+entry.title.text.encode(sys.getfilesystemencoding()))
+
+def isOwner(client, entry, targetLogin):
+	aclFeed = client.GetAclPermissions(entry.resource_id.text)
+	for acl in aclFeed.entry:
+		if acl.role.value == "owner" and acl.scope.value== targetLogin:
+			return True
+	return False
+
+def makeCopy(client, entry, oldLogin, newLogin):
+	if canTransferOwnership(entry):
+		LOG.info("Copying : "+str(entry.title.text.encode(sys.getfilesystemencoding())))
+		if not args.dryRun:
+			duplicated_entry = client.Copy(entry, entry.title.text.encode(sys.getfilesystemencoding()))
+			stats['copied']+=1
+			#now recopying same righs on duplicated entry
+			aclFeed = client.GetAclPermissions(entry.resource_id.text)
+			for acl in aclFeed.entry:
+				if acl.scope.value==oldLogin:
+					continue
+				if acl.scope.value==newLogin:
+					continue
+				LOG.info("        duplicating acl : "+str(acl.scope.value)+' ('+str(acl.scope.type)+') is '+str(acl.role.value)+' from '+entry.title.text.encode(sys.getfilesystemencoding())+" to "+duplicated_entry.title.text.encode(sys.getfilesystemencoding()))
+
+				newScope = gdata.acl.data.AclScope(value=str(acl.scope.value), type='user')
+				newRole = gdata.acl.data.AclRole(value=str(acl.role.value))
+				newAcl_entry = gdata.docs.data.Acl(scope=newScope, role=newRole)
+				LOG.info("        dup=             : "+str(newAcl_entry.scope.value)+' ('+str(newAcl_entry.scope.type)+') is '+str(newAcl_entry.role.value))
+				created_acl_entry = client.Post(newAcl_entry, duplicated_entry.GetAclLink().href)
+	else:
+		LOG.error("Manual copy required for "+entry.title.text.encode(sys.getfilesystemencoding())+" of type : "+entry.getDocumentType())
+		raw_input("Press a key when done")
+
 
 def makeReportFile(storeFolder):
 	return os.path.join(os.path.abspath(storeFolder), "IMPORTANT-A LIRE-ST.txt")
@@ -277,21 +255,17 @@ def forceFolder(dir):
 #http://code.google.com/intl/fr/apis/documents/docs/3.0/developers_guide_python.html#CopyingDocs
 if __name__ == '__main__':
 	signal.signal(signal.SIGINT, signal_handler)
-	print ("Google Docs Backup v. %s"%__version)
+	print ("Google Docs Transfer v. %s"%__version)
 	print ("Python version %s [%s]"% (sys.version, platform.python_version()))
-	print ("Inspired (but with different approach) by GDataCopier, http://gdatacopier.googlecode.com/")
-	print ("Primary developped when migrating from Google Account to Google Apps Account (many docs of many users to migrate)")
-	parser = argparse.ArgumentParser(description='Standard exemple : gdocsbackup.py -l jdoe@gmail.com ',epilog="Have fun!")
+	print ("Developped while migrating documents of standard Google accounts to Google Apps Domain accounts")
+	parser = argparse.ArgumentParser(description='Standard exemple : gtransfer.py -l olduser%domain.com@gtempaccount.com -o olduser@domain.com',epilog="Have fun!")
 	parser.add_argument('-l', '--login')
-	parser.add_argument('-p', '--password', help="Warning : the password could be stored in your console history (OS dependant). You'd better not use the option and enter your password when asked by the program")
 	parser.add_argument('-d', '--directory', help="Where to store you document. If not provided, will use a default localtion based on login and date")
-	parser.add_argument('-f', '--flat', action="store_true", default=False, help="when activated, don't store docs in collections")
-	parser.add_argument('-i', '--ignore', action="store_true", default=False)
 	parser.add_argument('-v', '--verbose', action = 'store_true', dest = 'verbose', default = False,	help = 'increase verbosity')	
 	parser.add_argument('-u', '--usage', action="store_true", default=False, help="show help and exit")
 	parser.add_argument('-U', '--update', action="store_true", default=False, help="Self update from "+__update_url+" then exit")
-	parser.add_argument('-s', '--skip', action="store_true", default=False, help="Skip unknown content type (data will NOT be copied!")
-	parser.add_argument('-D', '--debug', action="store_true", default=False, help="Active full debug")
+	parser.add_argument('-o', '--newOwner', help="New owner of documents")
+	parser.add_argument('--dryRun',action="store_true", default=False,  help="dry run : no copy, no share. Read only!")
 	
 	args = parser.parse_args()
 
@@ -308,62 +282,80 @@ if __name__ == '__main__':
 		print ("to ")
 		print ("%s"%  __file__)	
 		raw_input("Press ENTER to overwrite current version, or CTRL+C to exit now!")
-		urllib.urlretrieve(__update_url, __file__)
+		urllib.urlretrieve(__update_url, __firaw_inputle__)
 		print ("New version installed in %s"%__file__)
 		sys.exit(0)
 
-	if not args.login:
-		#python 3 : input, python 2.x : raw_input
-		if sys.version_info >= (3, 0):
-			args.login = input("Username [%s]: " % getpass.getuser())
-		else:
-			args.login = raw_input("Username [%s]: " % getpass.getuser())
-		if not args.login:
-			args.login=getpass.getuser()
-	if not args.password:
-		args.password=getpass.getpass()
-		if not args.password:
-			print ("Password is mandatory")
-			sys.exit(1)
+	oldOwnerPassword = getpass.getpass("Old (current) owner password ("+args.login+"): ")
+	newOwnerPassword = getpass.getpass("New owner password ("+args.newOwner+"): " )
 			
 	if not args.directory:
-		args.directory=str(datetime.date.today())+"_googledocs_backup_"+args.login
+		args.directory=str(datetime.date.today())+"_googledocs_transfer_"+args.login
 	folder = forceFolder(args.directory)
 	LOG.info ("Google Docs Transfer v. %s"%__version)
-	LOG.info ("Nous sommes le               : %s" % datetime.date.today())
-	LOG.info ("Authentification utilisée    : %s:xx" % args.login)
-	LOG.info ("Répertoire de stockage       : "+ os.path.abspath(folder))
-	LOG.info ("Stockage dans la collection  : "+ ("NON" if args.flat else "OUI"))
-	if args.flat==False:
-		LOG.info ("Ignorer si multi collections : "+ ("IGNORER" if args.ignore else "LANCER UNE ERREUR"))
+	LOG.info ("Current date                 : %s" % datetime.date.today())
+	LOG.info ("Store folder                 : "+ os.path.abspath(folder))
 	if args.verbose==True:
 		LOG.info("Verbose Mode                 : ACTIVE")
-	if args.debug==True:
-		LOG.info("Debug Mode                   : ACTIVE")
-	if args.skip==True:
-		LOG.info("Skip unknown content         : ACTIVE")
+	if args.dryRun:
+		LOG.info("***** Dry Run enabled - No modification will be made to your documents *****")	
+	LOG.info("New owner of my documents     : "+args.newOwner)
 	LOG.info ("System Encoding              : %s"%sys.getfilesystemencoding())
-	LOG.info ("====================================================================")
-	LOG.info ("ATTENTION : SEUL LES DOCUMENTS APPARTENANT A "+args.login+" SERONT RECUPERES !!!")
-	LOG.info ("====================================================================")
-	raw_input("ENTREE pour continuer, ou CTRL+C pour annuler...")
-	LOG.info ("Connexion sur le serveur Google...")
-	client = gdata.docs.client.DocsClient(source="jgraglia-gdocsbackup-v1")
-	client.ssl = True 
-	client.http_client.debug = args.debug
-	client.ClientLogin(args.login, args.password, client.source);
-	LOG.info ("    -> succès")
-	LOG.info ("Récupération de la liste de tous les documents de %s"%args.login)
-	feed = client.GetDocList(uri='/feeds/default/private/full/')
-	spreadsheets_client = gdata.spreadsheet.service.SpreadsheetsService()
-	spreadsheets_client.ClientLogin(args.login, args.password)
-	#client.auth_token = gdata.gauth.ClientLoginToken(spreadsheets_client.GetClientLoginToken())
-	login = args.login
-	stats = transferDocument(client, client.auth_token, gdata.gauth.ClientLoginToken(spreadsheets_client.GetClientLoginToken()), feed, folder, args.flat, args.ignore)
-	LOG.info ("Storing log in backup folder (contains important ownership and share infos, that you could use when re-importing documents)")
-	shutil.copy2('output.log', folder+"/output.log")
-	LOG.info (os.path.abspath(folder))
-	if stats['error']>0:
-		LOG.error("ERROR ! "+str(stats['error'])+" error(s) occured, some document aren't stored. See output.log for details")
-	else:
-		LOG.info ("SUCCESS !")
+	LOG.info("===============================================")
+	LOG.info("will transfer ALL docs and SHARE of "+args.login+" to accouunt "+args.newOwner)
+	LOG.info("===============================================")
+	raw_input("ENTER TO CONTINUE OR CTRL+C TO CANCEL...")
+	LOG.info ("Connecting with google account  : "+args.login)
+	oldOwner = gdata.docs.client.DocsClient(source="jgraglia-gdocsbackup-v1")
+	oldOwner.ssl = True 
+	oldOwner.http_client.debug = args.verbose
+	oldOwner.ClientLogin(args.login, oldOwnerPassword, oldOwner.source);
+	LOG.info ("    -> success")
+
+	LOG.info ("Connecting with google account  : "+args.newOwner)
+	newOwner = gdata.docs.client.DocsClient(source="jgraglia-gdocsbackup-v1")
+	newOwner.ssl = True 
+	newOwner.http_client.debug = args.verbose
+	newOwner.ClientLogin(args.newOwner, newOwnerPassword, newOwner.source);
+	LOG.info ("    -> success")
+
+	LOG.info ("Retreiving all docs of %s"%args.login)
+	oldDocsFeed = oldOwner.GetDocList(uri='/feeds/default/private/full/')
+	
+	doStep1=True
+	doStep2=True
+	doStep3=True
+	doStep4=True
+	stats = {'removeoldownerright':0, 'addwriter':0, 'copied':0, 'step4':0}
+	if doStep1:
+		LOG.info("1/ Adding writer rigthts to "+args.newOwner+" to ALL documents of "+args.login)
+		for entry in oldDocsFeed.entry:
+			addWriterShare(oldOwner, entry, args.newOwner)
+		LOG.info("Stats:"+str(stats))
+
+	if doStep2:
+		LOG.info("2/ Removing "+args.login+" rights on ALL non owned documents")
+		oldDocsFeed = oldOwner.GetDocList(uri='/feeds/default/private/full/')
+		for entry in oldDocsFeed.entry:
+			removeAllRightsIfNotOwned(oldOwner, entry, args.login)
+		LOG.info("Stats:"+str(stats))
+
+	if doStep3:
+		LOG.info("3/ Copying docs owned by "+args.login+" on account "+args.newOwner+" in order to get ownership (can't transfer ownership among domains")
+		newDocsFeed = newOwner.GetDocList(uri='/feeds/default/private/full/')
+		for entry in newDocsFeed.entry:
+			if isOwner(newOwner, entry, args.login):
+				LOG.info("Processing doc owned by "+args.login+" : "+entry.title.text.encode(sys.getfilesystemencoding()))
+				makeCopy(newOwner, entry, args.login,args.newOwner)
+		LOG.info("Stats:"+str(stats))
+
+	if doStep4:
+		LOG.info("4/ Removing all remaing rights of "+args.login)
+		oldDocsFeed = oldOwner.GetDocList(uri='/feeds/default/private/full/')
+		for entry in oldDocsFeed.entry:
+			removeAllRightsExceptMine(oldOwner, entry, args.login)
+	
+	if args.dryRun:
+		LOG.info("DRY RUN : no modifications where applied!")
+	LOG.info("Stats:"+str(stats))
+	LOG.info ("Done !")
